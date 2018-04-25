a792dee0334f80d3fb65b349af6f0fa249a3d507
PJB <pjbresnahan@gmail.com>
2018-04-25 12:21:26 -0700

    Moved keys and updated code that depends on them

--- quickstart.py
+++ quickstart.py
@@ -8,10 +8,10 @@ from oauth2client import file, client, tools
 
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-store = file.Storage('credentials.json')
+store = file.Storage('../Online-Access/credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('../Online-Access/client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
