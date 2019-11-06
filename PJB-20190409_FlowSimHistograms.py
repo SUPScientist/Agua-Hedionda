405fb6a40ed5d356e9b7f9579524d5b9be14194d
pjb-mbp15 <pjbresnahan@gmail.com>
2019-11-06 11:48:17 -0800

    Generic catchup plus add plotly/ERDDAP plotter

--- PJB-20190409_FlowSimHistograms.py
+++ PJB-20190409_FlowSimHistograms.py
@@ -1,46 +1,64 @@
-
 #%%
-
-# In[1]:
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-
-# ### Read in flow simulation data for various endcaps and visualize to see variability in performance
-
-# In[10]:
-
-
+### Read in flow simulation data for various endcaps and visualize to see variability in performance
 cwd = os.getcwd()
-filename = 'V3.2_Flow_Sim.xlsx'
+
+## Printed part
+### Read in data
+filename = 'V3.2_Flow_Sim_printed.xlsx'
 filepath = os.path.join(cwd, 'Data', filename)
 
 df_3p2_3D = pd.read_excel(filepath)
 
+### Format and clean data
 df_3p2_3D.drop(index = 0, inplace = True) # bad row
 df_3p2_3D.dropna(axis = 0, inplace = True)
 df_3p2_3D['Residence time [s]'] = df_3p2_3D['Residence time [s]'].astype('float')
 
+### Plot
+fig, axs = plt.subplots()
+axs.hist(df_3p2_3D['Residence time [s]'], bins = 100, label = filename)
+axs.set_xlabel('Residence Time (s)')
+axs.set_ylabel('Count')
 
-# In[11]:
+## Printed part
+### Read in data
+filename = 'V3.2_Flow_Sim_printed.xlsx'
+filepath = os.path.join(cwd, 'Data', filename)
+
+df_3p2_3D = pd.read_excel(filepath)
+
+### Format and clean data
+df_3p2_3D.drop(index = 0, inplace = True) # bad row
+df_3p2_3D.dropna(axis = 0, inplace = True)
+df_3p2_3D['Residence time [s]'] = df_3p2_3D['Residence time [s]'].astype('float')
+
+### Plot
+fig, axs = plt.subplots()
+axs.hist(df_3p2_3D['Residence time [s]'], bins = 100, label = filename)
+axs.set_xlabel('Residence Time (s)')
+axs.set_ylabel('Count')
 
 
-df_3p2_3D.head(100)
+## Machined part
+### Read in data
+filename = 'V3.2_Flow_Sim_machined.xlsx'
+filepath = os.path.join(cwd, 'Data', filename)
 
+df_3p2_CNC = pd.read_excel(filepath)
 
-# In[12]:
+### Format and clean data
+df_3p2_CNC.drop(index = 0, inplace = True) # bad row
+df_3p2_CNC.dropna(axis = 0, inplace = True)
+df_3p2_CNC['Residence time [s]'] = df_3p2_CNC['Residence time [s]'].astype('float')
 
+### Plot
+axs.hist(df_3p2_CNC['Residence time [s]'], bins = 100, label = filename)
+axs.set_xlabel('Residence Time (s)')
+axs.set_ylabel('Count')
 
-df_3p2_3D.shape
-
-
-# In[15]:
-
-
-fig2 = plt.figure(figsize = [8, 8])
-df_3p2_3D.hist(bins = 100)
-
+plt.legend()
