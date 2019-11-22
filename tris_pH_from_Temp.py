e52d2ee0fe57ba3edb3d23a62968485d0474bada
pjb-mbp15 <pjbresnahan@gmail.com>
2019-11-21 16:36:18 -0800

    Reformat print statements

--- tris_pH_from_Temp.py
+++ tris_pH_from_Temp.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-print("Temperature: ")
+print("Enter temperature (deg C): ")
 T_C = float(input())
+
 T = T_C+273.15
 S = 35
 # T =
 
 pHT = (11911.08 - 18.2499*S - 0.039336*S**2)/T - 366.27059 + 0.53993607*S + 0.00016329*S**2 + (64.52243 - 0.084041*S)*np.log(T) - 0.11149858*T
 
-print(pHT)
+print("Tris pH at {} is calculated as {}".format(T_C, pHT))
 
 # T25 = 25+273.15
 # T = 20+273.15
